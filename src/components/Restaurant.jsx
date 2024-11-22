ad49a983023425882b1b983547dcb268f22c60ec
Shree-Ram-R <sriram00018@gmail.com>
2024-11-22 20:24:02 +0530

    first

--- src/components/Restaurant.jsx
+++ src/components/Restaurant.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaStar } from 'react-icons/fa';
 
 const Restaurant = () => {
     const resta = [
@@ -6,81 +7,106 @@ const Restaurant = () => {
           id: 1,
     
           image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/7/17/ada9dd71-dbe7-4e21-928a-3388f7cef91b_559820.jpg",
+          name:"Pizza Hut",
+          rating:4.2,
     
         },
         {
           id: 2,
     
           image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/5116a385bac0548e06c33c08350fbf11",
-    
+          name:"The Belgian Waffle Co.",
+          rating:4.6,
         },
         {
           id: 3,
     
           image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/10/24/06315894-32fc-4d85-9bc6-9360f7827b6d_80481.JPG",
-    
+          name:"Domino's Pizza",
+          rating:4.3,
         },
         {
           id: 4,
     
           image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/10/25/844237e8-affb-4939-a6cd-3a7b02bec8e8_350017.jpg",
-    
+          name:"Subway",
+          rating:4.0,
         },
         {
           id: 5,
     
-          image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/11/5/f4ac5814-9ffc-4921-9f3c-216d1d49461f_81246.JPG"
-          
+          image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/11/5/f4ac5814-9ffc-4921-9f3c-216d1d49461f_81246.JPG",
+          name:"KFC",
+          rating:4.3,
         },
         {
           id: 6,
         
           image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/wqbd9zvrtiwxyqhnmliy",
-         
+          name:"Ambur Star Briyani",
+          rating:4.4,
         },
         {
           id: 7,
          
           image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/7/8/be13f562-38aa-41b1-aaed-056ece24a711_919309.jpg",
-          
+          name:"Domino's Chicken Fiesta",
+          rating:4.2,
         },
         {
           id: 8,
       
           image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/10/11/45996e90-9d21-48b4-95e9-38b9953e0a4c_782308.jpg",
-         
+          name:"Popeyes",
+          rating:4.5,
         },
         {
           id: 9,
           image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/9/3/040a6212-eeb7-40d2-b8b7-d705599b97b5_664040.jpg",
+          name:"Burger King",
+          rating:4.2,
         },
         {
           id: 10,
           image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/c583ca6ce40b426797a78ae2ac91f2ec",
+          name:"Faasos-Signature Wraps,Rolls&....",
+          rating:4.2,
         },
         {
           id: 11,
           image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/h8uh8fb7gvbipfhpbuzp",
+          name:"Baskin Robbins-Ice Cream...",
+          rating:4.2,
         },
         {
           id: 12,
           image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/f60a7f94250ee888893cf2c1a546e732",
+          name:"Salem RR Briyani",
+          rating:4.2,
         },
         {
           id: 13,
           image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/7/28/119c8f39-b82b-43b6-8d06-e0eb959a33d7_364920.JPG",
+          name:"Faasos-Wraps,Rolls&....",
+          rating:4.2,
         },
         {
           id: 14,
           image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/aabvgaoaldfyo4ckxd81",
+          name:"Sandwich Club",
+          rating:4.5,
         },
         {
           id: 15,
           image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/cf63bec75de1e2ca7809e07298f0fd69",
+          name:"Caake Bee",
+          rating:4.6,
         },
         {
           id: 16,
           image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/11/13/8260b952-9a70-4afd-a52f-86d971bc0ca4_696605.jpg",
+          name:"Ovenfresh Cakes and Desserts",
+          rating:4.4,
         }
         
       ];
@@ -90,7 +116,8 @@ const Restaurant = () => {
         resta.map((res)=>(
           <div key={res.id} className="product">
             <img className='res-img' src={res.image} alt={res.name} />
-            <h4>Pizza Hut</h4>
+            <h4>{res.name}</h4>
+            <h4><FaStar/>{res.rating}</h4>
           </div>
         ))
       }
