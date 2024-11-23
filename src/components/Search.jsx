b621f1a553c01628da8a4b66421d7f85820d6820
Shree-Ram-R <sriram00018@gmail.com>
2024-11-23 15:38:00 +0530

    commit

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Cuisines from './Cuisines'
+import { FaSearch } from 'react-icons/fa'
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className='searchPage'>
+      <div className="search">
+        <input type="text"  placeholder='Search For Restaurant and Foods'/>
+        <div className="searchIcon"><FaSearch/></div>
+        
+      </div>
+      <h3 className='res'>Popular Cuisines</h3>
+      <Cuisines/>
+    </div>
   )
 }
 
