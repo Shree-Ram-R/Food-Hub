e825c06d111eda4c8629e9912848c84384ef6ab1
Shree-Ram-R <sriram00018@gmail.com>
2024-11-24 16:46:57 +0530

    cru

--- src/components/AddCart.jsx
+++ src/components/AddCart.jsx
@@ -1,21 +1,53 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import "./AddCart.css"
 const AddCart = ({cart,setCart}) => {
   const [total,setTotal]=useState(0);
+  useEffect(()=>{
+    setTotal(cart.reduce((acc,curr)=>acc + parseInt(curr.price)*curr.item,0));
+  },[cart]);
+
+  const handleQuantityChange = (id, action) => {
+    setCart((prevCart) => {
+      const updatedCart = prevCart.map((item) => {
+        if (item.id === id) {
+          // Update item quantity
+          return { ...item, item: action === 'increase' ? item.item + 1 : item.item - 1 };
+        }
+        return item;
+      });
+      return updatedCart;
+    });
+  };
   return (
     <>
     <h1 className='heading'>Cart Products</h1>
     <div className='cart-container'>
       {
         cart.map((res)=>(
-          <div className="cart-product">
-        <div className="img">
+          <div key={res.id} className="cart-product">
+            <div className="pro">
+            <div className="img">
           <img src={res.image} alt="img" />
         </div>
         <div className="cart-product-details">
           <h3>{res.food}</h3>
           <p>Price Rs: {res.price}</p>
         </div>
+        
+        </div>
+        <div className='quantity'>
+          <h4>Quantity</h4>
+          <button
+                onClick={() => handleQuantityChange(res.id, 'decrease')}
+                disabled={res.item <= 1} // Disable the button if quantity is 1
+              >
+                -
+              </button>
+              <span>{res.item}</span>
+              <button onClick={() => handleQuantityChange(res.id, 'increase')}>+</button>
+           
+        </div>
+        
       </div>
         ))
       }
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,23 +1,44 @@
+
 import React from 'react'
+import  { useState } from 'react'
 import { FaShippingFast, FaStar } from 'react-icons/fa';
+
 const Product = ({res,cart,setCart}) => {
-    const addCart=()=>{
-        setCart([...cart,res])
-    }
-    const removeCart=()=>{
-        setCart(cart.filter((c)=>c.id!==res.id))
-    }
+    
+    
+    const addToCart = (product) => {
+        setCart([...cart, product]);
+       
+    };
+
+    const removeFromCart = (productId) => {
+        setCart(cart.filter((item) => item.id !== productId));
+       
+    };
+
+    const isInCart = (productId) => {
+        return cart.some((item) => item.id === productId);
+    };
+    
   return (
     <>
         <img className='res-img' src={res.image} alt={res.name} />
-        <h5 className='price'>{res.price}</h5>
+        <h5 className='price'>₹{res.price}</h5>
         <div className="foodPrice"><h4>{res.food}</h4>
         </div>
         
         <div className="namRat"> <h4>{res.name}</h4>
         <h4><FaStar/>{res.rating}</h4></div>
-        {cart.includes(res)?(<button  className='remove' onClick={removeCart}>Remove Cart</button>):(<button className='add' onClick={addCart}>Add Cart</button>)}
-    </>
+        <button  className={isInCart(res.id) ? "remove" : "add"}
+                    key={res.id}
+                    onClick={() =>
+                        isInCart(res.id)
+                            ? removeFromCart(res.id)
+                            : addToCart(res)
+                    }
+                >
+                    {isInCart(res.id) ? "Remove from Cart" : "Add to Cart"}
+                </button></>
   )
 }
 
